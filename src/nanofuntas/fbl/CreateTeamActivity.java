f5edea236bb7dd215389f1183b0b4522ee4f4e07
WU YONGSHOU <footballlifer@hotmail.com>
2013-12-11 00:20:17 +0900

    create team discard button

--- src/nanofuntas/fbl/CreateTeamActivity.java
+++ src/nanofuntas/fbl/CreateTeamActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -16,10 +17,11 @@ public class CreateTeamActivity extends Activity {
 	private final String TAG = "CreateTeamActivity";
 	
 	private EditText mTeamName;
-	private Button mCreateTeam;
 	
 	private SharedPreferences settings;
 	private SharedPreferences.Editor editor;
+	
+	private long UID;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,29 +29,25 @@ public class CreateTeamActivity extends Activity {
 		setContentView(R.layout.activity_create_team);
 		
 		mTeamName = (EditText) findViewById(R.id.team_name_create);
-		mCreateTeam = (Button) findViewById(R.id.create_team);
 		
 		settings = getSharedPreferences(Config.FBL_SETTINGS, 0);
     	editor = settings.edit();
-    	final long UID = settings.getLong(Config.KEY_UID, 0);
-
-    	mCreateTeam.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (DEBUG) Log.d(TAG, "Create Team clicked!");
-				
-				String teamName = mTeamName.getText().toString();
-				long tid = ServerIface.createTeam(UID, teamName);
-				
-				editor.putLong(Config.KEY_TID, tid);
-				editor.commit();
-				
-				Toast.makeText(getApplication(), 
-						"Team Created, TID:" + Long.toString(tid), Toast.LENGTH_SHORT).show();
-			}
-		});
+    	UID = settings.getLong(Config.KEY_UID, 0);
 	}
 
+	private void createTeam() {
+		if (DEBUG) Log.d(TAG, "Create Team clicked!");
+		
+		String teamName = mTeamName.getText().toString();
+		long tid = ServerIface.createTeam(UID, teamName);
+		
+		editor.putLong(Config.KEY_TID, tid);
+		editor.commit();
+		
+		Toast.makeText(getApplication(), 
+				"Team Created, TID:" + Long.toString(tid), Toast.LENGTH_SHORT).show();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -57,4 +55,15 @@ public class CreateTeamActivity extends Activity {
 		return true;
 	}
 
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {        
+        switch (item.getItemId()) {
+        case R.id.menu_create:
+        	createTeam();
+        	return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+	
 }
